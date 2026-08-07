81bc15b338512a1f47b2eb20ef89dc218c49fa6f
mukul <mukulkumarlkr9097@gmail.com>
2026-08-07 22:23:52 +0530

    Find largest number

---
+++ 1_laegest_NO.py
@@ -0,0 +1,11 @@
+
+def largest_no(n):
+    largest = n[0]
+    for i in n:
+        if i > largest:
+            largest = i
+    return largest
+
+n = [int(x) for x in input("Enter the elements : ").split()]
+result = largest_no(n)
+print("The largest number is : ", result)
