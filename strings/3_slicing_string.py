b9051053222705ea4f8cf825bc96aefc2ed06f47
mukul <mukulkumarlkr9097@gmail.com>
2026-08-18 14:48:46 +0530

    slicing string

---
+++ strings/3_slicing_string.py
@@ -0,0 +1,14 @@
+#if second number - slice - up to but not including
+
+s = 'Mukul Python'
+print(s[0:4])
+
+#if second number -beyond the end of string, it stop at the end of string ended
+print(s[6:20])
+
+#if we eliminate first and second  number it assumed to be the the beginning or end of the string resp.
+print(s[:2])
+
+print(s[6:])
+
+print(s[:])
