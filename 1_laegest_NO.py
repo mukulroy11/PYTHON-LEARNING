6c8ed19e57a9d7803807f01bb60e74b220fe336b
mukul <mukulkumarlkr9097@gmail.com>
2026-08-18 14:19:39 +0530

    Looping Through String

--- 1_laegest_NO.py
+++ 1_laegest_NO.py
@@ -6,6 +6,7 @@ def largest_no(n):
             largest = i
     return largest
 
-n = [int(x) for x in input("Enter the elements : ").split()]
+#n = [int(x) for x in input("Enter the elements : ").split()]
+n = [3, 5, 7, 2, 8]
 result = largest_no(n)
-print("The largest number is : ", result)
+print("The largest number is :", result)
---
+++ looping_through_string.py
@@ -0,0 +1,6 @@
+fruit = 'banana'
+i = 0
+while i < len(fruit):
+    letter = fruit[i]
+    print(i, letter)
+    i = i + 1
