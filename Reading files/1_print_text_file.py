22059baffed931bd325b16e551e347e8d900bba9
mukul <mukulkumarlkr9097@gmail.com>
2026-08-19 16:00:47 +0530

    remove new line using rstrip function

--- Reading files/1_print_text_file.py
+++ Reading files/1_print_text_file.py
@@ -9,5 +9,12 @@ for line in fhand:
 print('Line Count:', count)
 
 fhand = open('E:\PYTHON LEARNING\Reading files\m.txt')
+#fhand = open('E:\PYTHON LEARNING\Reading files\m.txt', 'r')
 inp = fhand.read()
 print(len(inp))
+
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip() # rstrip() is used to remove the extra whitespace or newline character from the end of the line
+    if line.startswith('From:'):
+        print(line)
