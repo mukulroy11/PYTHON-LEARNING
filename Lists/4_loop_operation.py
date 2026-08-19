b583f334e36df666bc66719ba57c2be5cbdfe94e
mukul <mukulkumarlkr9097@gmail.com>
2026-08-19 22:18:59 +0530

    concatenation and slice

---
+++ Lists/4_loop_operation.py
@@ -0,0 +1,12 @@
+# concating list
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c) # Output: [1, 2, 3, 4, 5, 6]
+
+#slice
+t = [9, 41, 12, 3, 74, 15]
+print(t[1:3]) # Output: [41, 12]
+print(t[:4]) # Output: [9, 41, 12, 3]
+print(t[3:]) # Output: [3, 74, 15]
+print(t[:]) # Output: [9, 41, 12, 3, 74, 15]
