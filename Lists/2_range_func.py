b88862c2da47f4ebf909c1b12a3d5a899610dd32
mukul <mukulkumarlkr9097@gmail.com>
2026-08-19 22:13:57 +0530

    range

---
+++ Lists/2_range_func.py
@@ -0,0 +1,7 @@
+print(range(10)) # Output: range(0, 10 )
+
+#range function return alist of numbers
+
+friends = ['mukul', 'sahil', 'rahul', 'rohit', 'sachin']
+print(len(friends)) # Output: 5
+print(range(len(friends))) # Output: range(0, 5)
