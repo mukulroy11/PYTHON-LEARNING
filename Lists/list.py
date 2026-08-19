41c8f6f0e8c139556f42b754043b1182d3281887
mukul <mukulkumarlkr9097@gmail.com>
2026-08-19 22:08:50 +0530

    list

---
+++ Lists/list.py
@@ -0,0 +1,10 @@
+x = ['mukul', 'sahil', 'rahul', 'rohit', 'sachin']
+for i in x:
+    print('happy new year : ', i)
+
+y = "Banana"
+#y[0]='b' # This will cause an error because strings are immutable in Python
+
+l = [2, 4, 6, 8, 10]
+l[3] = 9 # This will change the value at index 3 from 8 to 9
+print(l) # Output: [2, 4, 6, 9, 10]
