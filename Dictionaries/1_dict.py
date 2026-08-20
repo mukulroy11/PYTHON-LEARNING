e21ffb43a11a5b8de5dda5ce2d57776538d0b275
mukul <mukulkumarlkr9097@gmail.com>
2026-08-20 09:32:13 +0530

    study dictionaries

---
+++ Dictionaries/1_dict.py
@@ -0,0 +1,8 @@
+purse = dict()  # or purse = {}
+purse['money'] = 12
+purse['candy'] = 3
+print(purse)  # Output: {'money': 12, 'candy': 3}
+print(purse['money'])  # Output: 12
+
+purse['candy'] = purse['candy'] + 2
+print(purse)  # Output: {'money': 12, 'candy': 5}
