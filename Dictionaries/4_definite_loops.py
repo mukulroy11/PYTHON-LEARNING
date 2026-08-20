ff164ac9b93d6c5876d1329c41836ee84bfb92b1
mukul <mukulkumarlkr9097@gmail.com>
2026-08-20 09:53:25 +0530

    two iterration variable

--- Dictionaries/4_definite_loops.py
+++ Dictionaries/4_definite_loops.py
@@ -7,3 +7,7 @@ for key in counts:
 print(counts.keys()) # Output: dict_keys(['mukul', 'sachin', 'rahul'])
 print(counts.values()) # Output: dict_values([1, 2, 1])
 print(counts.items()) # Output: dict_items([('mukul', 1), ('sachin', 2), ('rahul', 1)])
+
+#two  iteration variables to iterate over the items in a dictionary
+for k,v in counts.items():
+    print(k,v) # Output: mukul 1 sachin 2 rahul 1
