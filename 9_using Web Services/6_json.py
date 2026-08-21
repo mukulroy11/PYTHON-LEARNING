c3731a5f8498d6751d03b093b98a030bb5b7956d
mukul <mukulkumarlkr9097@gmail.com>
2026-08-21 11:38:22 +0530

    using lists

--- 9_using Web Services/6_json.py
+++ 9_using Web Services/6_json.py
@@ -5,8 +5,16 @@ data = '''[
         "x" : "2",
         "name" : "mukul"
     },
-    {   "id" : "007"
-        "x" : "7"
+    {   "id" : "007",
+        "x" : "7",
         "name" : "Rahul"
     }
 ]'''
+
+info = json.loads(data)
+print('User count:', len(info))
+for item in info:
+    print("Name:", item['name'])
+    print("Id", item['id'])
+    print('Attribute', item['x'])
+
